2470505bb271aa4b89ba62ae566f6ec31fb6be22
Hayfa SNEN <snen.hayfa@gmail.com>
2018-04-20 17:38:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="author" content="Hayfa SNEN">
+    		<meta name="mail" content="snen.hayfa@gmail.com">
 		
 		<title>MultiChain Web Explorer</title>
 		<link type='image/png' rel='icon' href='img/logo_multichain.png' />
